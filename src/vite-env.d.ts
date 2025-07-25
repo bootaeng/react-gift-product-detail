c068b8f49083dbaadcd88abef11ade52dea1b87a
Kwon Taehyun <xogus0065@naver.com>
2025-07-25 14:57:28 +0900

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,7 @@
 /// <reference types="vite/client" />
-<<<<<<< HEAD
 
 declare module '@/styles/reset' {
   import { SerializedStyles } from '@emotion/react'
   export const globalReset: SerializedStyles
 }
 
-=======
->>>>>>> 5628913b9fe5647f65c620d00b2b3d263d7fd596
